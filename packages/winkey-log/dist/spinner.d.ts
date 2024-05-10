fa8b71f885f2fbbc79e1a2db3818b3f9c3cf4acc
Ricardom520 <1781218530@qq.com>
2024-05-10 10:20:02 +0800

    test: 测试

--- packages/winkey-log/dist/spinner.d.ts
+++ packages/winkey-log/dist/spinner.d.ts
@@ -1,20 +1,20 @@
-export declare const spinner: any;
+export declare const spinner: any
 declare class LogSpinner {
-    private spinner;
-    private timer;
-    private speedTime;
-    infoSpinner(text: string): void;
-    startSpinner(text: string): void;
-    succeedSpiner(text?: string): void;
-    failSpinner(text?: string): void;
-    addSpinner(text?: string): void;
-    updateSpinner(text?: string): void;
-    warnSpinner(text?: string): void;
-    cmdSpinner(text?: string): void;
-    waitSpinner(text?: string): void;
-    tipSpinner(text?: string): void;
-    correctSpinner(text?: string): void;
-    deleteSpinner(text?: string): void;
-    finishSpinner(text?: string, status?: number): void;
+  private spinner
+  private timer
+  private speedTime
+  infoSpinner(text: string): void
+  startSpinner(text: string): void
+  succeedSpiner(text?: string): void
+  failSpinner(text?: string): void
+  addSpinner(text?: string): void
+  updateSpinner(text?: string): void
+  warnSpinner(text?: string): void
+  cmdSpinner(text?: string): void
+  waitSpinner(text?: string): void
+  tipSpinner(text?: string): void
+  correctSpinner(text?: string): void
+  deleteSpinner(text?: string): void
+  finishSpinner(text?: string, status?: number): void
 }
-export default LogSpinner;
+export default LogSpinner
