ea5d03cbb211c0bc3a552c0ca8a928913e9617af
Ricardom520 <1781218530@qq.com>
2024-05-11 14:49:50 +0800

    chore: 备份

--- packages/winkey-init-project-seed-npm/src/seeds/base/rollup.config.js
+++ packages/winkey-init-project-seed-npm/src/seeds/base/rollup.config.js
@@ -1,49 +1,46 @@
-import nodeResolve from "@rollup/plugin-node-resolve"
-import commonjs from "@rollup/plugin-commonjs"
-import typescript from "@rollup/plugin-typescript"
-import json from "@rollup/plugin-json"
-import del from "rollup-plugin-delete"
+import nodeResolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import json from '@rollup/plugin-json'
+import del from 'rollup-plugin-delete'
 import pkg from './package.json'
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: {
-      file: "dist/index.cjs.js",
-      format: "cjs",
-      entryFileNames: "[name].cjs.js",
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+      entryFileNames: '[name].cjs.js'
     },
-    external: [
-      ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.peerDependencies || {})
-    ],
+    external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
     plugins: [
       del({
-        targets: ["dist/**/*"],
+        targets: ['dist/**/*']
       }),
       nodeResolve(),
       commonjs(),
       json(),
       typescript({
-        tsconfig: "./tsconfig.json",
-      }),
-    ],
+        tsconfig: './tsconfig.json'
+      })
+    ]
     // external: [...] // 外部引用的库，不要打包，用于处理 peerDependencies
   },
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: {
-      file: "dist/index.esm.js",
-      format: "esm",
-      entryFileNames: "[name].esm.js",
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      entryFileNames: '[name].esm.js'
     },
     plugins: [
       nodeResolve(),
       commonjs(),
       json(),
       typescript({
-        tsconfig: "./tsconfig.json",
-      }),
-    ],
-  },
+        tsconfig: './tsconfig.json'
+      })
+    ]
+  }
 ]
