3d9c33c38b6d5cd7b9c2fb08d3b3eeeb2c2ea0d1
Ricardom520 <1781218530@qq.com>
2024-05-24 18:29:44 +0800

    test: 备份

--- packages/winkey-init-config-miniprogram/src/index.ts
+++ packages/winkey-init-config-miniprogram/src/index.ts
@@ -1,8 +1,8 @@
 import path from "path";
 import WkMiniProgram from "./wkMiniprogram";
-import fs from 'fs'
-import { logger, LogType } from 'winkey-log'
-import esbuild from 'esbuild'
+import fs from "fs";
+import { logger, LogType } from "winkey-log";
+import esbuild from "esbuild";
 
 export enum ExecType {
   /** 开发模式 */
@@ -12,30 +12,30 @@ export enum ExecType {
 }
 
 export async function initWinkeyConfig(type: ExecType, context?: string) {
-  const cwd = process.cwd()
-  const root = context ? path.resolve(cwd, context) : cwd
-  const configTsPath = path.resolve(root, "winkey.config.ts")
-  const configJsPath = path.resolve(root, "winkey.config.js")
-  const pkgPath = path.join(root, 'package.json')
+  const cwd = process.cwd();
+  const root = context ? path.resolve(cwd, context) : cwd;
+  const configTsPath = path.resolve(root, "winkey.config.ts");
+  const configJsPath = path.resolve(root, "winkey.config.js");
+  const pkgPath = path.join(root, "package.json");
 
   if (fs.existsSync(configTsPath)) {
-    const outfile = path.join(root, '_bat.config.js')
-    const pkg = require(pkgPath)
+    const outfile = path.join(root, "_bat.config.js");
+    const pkg = require(pkgPath);
     const external = Object.keys(pkg.dependencies || {})
-    .concat(Object.keys(pkg.devDependencies || {}))
-    .concat(['*.json', '*.js'])
+      .concat(Object.keys(pkg.devDependencies || {}))
+      .concat(["*.json", "*.js"]);
 
     const buildRes = await esbuild.build({
       sourceRoot: root,
       entryPoints: [configTsPath],
       bundle: true,
-      platform: 'node',
+      platform: "node",
       external,
-      outfile
-    })
+      outfile,
+    });
 
     if (buildRes.errors.length) {
-      logger(LogType.Error, buildRes.errors[0].text)
+      logger(LogType.Error, buildRes.errors[0].text);
     } else {
       // const config = await formatBatConfig({
       //   config: {
@@ -48,9 +48,8 @@ export async function initWinkeyConfig(type: ExecType, context?: string) {
       // r = [undefined, config]
     }
   } else if (fs.existsSync(configJsPath)) {
-
   } else {
-    logger(LogType.Info, '未找到配置文件')
+    logger(LogType.Info, "未找到配置文件");
   }
   const config = require(path.resolve(process.cwd(), "winkey.config.ts"));
 
